d00a9967e9e56cb0dc1b6d2275273ddb206b2161
merfohacev <74780443+merfohacev@users.noreply.github.com>
2023-12-21 03:56:10 +0700

    Update date.h

--- date.h
+++ date.h
@@ -7,17 +7,6 @@
 
 using namespace std;
 
-//struct Date {
-//
-//    int day;
-//    int month;
-//    int year;
-//
-//    Date() {
-//        day, month, year = 0;
-//    }
-//};
-
 struct Date {
     int day;
     int month;
@@ -25,20 +14,7 @@ struct Date {
 
     Date() : day(0), month(0), year(0) {}
 
-    // Конструктор для поддержки инициализации из списка
-    /*Date(std::initializer_list<int> list) {
-        if (list.size() == 3) {
-            auto it = list.begin();
-            year = *it;
-            ++it;
-            month = *it;
-            ++it;
-            day = *it;
-        }
-        else {
-            throw std::invalid_argument("Initializer list must contain three elements.");
-        }
-    }*/
+   
 };
 
 istream& operator >> (istream& ist, Date& date);
