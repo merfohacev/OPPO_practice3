5c6e2fb8292cd0ccf7f958ba271096dcd6181c07
merfohacev <74780443+merfohacev@users.noreply.github.com>
2023-12-21 03:56:27 +0700

    Update date.cpp

--- date.cpp
+++ date.cpp
@@ -5,20 +5,6 @@
 
 using namespace std;
 
-//istream& operator >> (istream& is, Date& date) {
-//    is >> date.year;
-//    is.get();
-//    is >> date.month;
-//    is.get();
-//    is >> date.day;
-//    return is;
-//}
-//
-//ostream& operator<< (ostream& os, Date& date) {
-//    os << date.year << "." << date.month << "." << date.day;
-//    return os;
-//}
-
 bool IsLeapYear(int year) {
     return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
@@ -31,14 +17,13 @@ std::istream& operator>>(std::istream& is, Date& date) {
         is.get();
         is >> date.day;
 
-        // Проверка на високосный год
+        
         if (date.month == 2 && date.day == 29 && !IsLeapYear(date.year)) {
             throw std::runtime_error("Invalid date: Not a leap year, but day is 29 in February.");
         }
 
     }
     catch (const std::exception& e) {
-        // Обработка ошибок чтения даты
         std::cerr << "Error while reading Date data: " << e.what() << std::endl;
     }
 
